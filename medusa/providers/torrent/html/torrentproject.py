a3e1242f281955e303fbe544e3adaace3988325d
supergonkas <matigonkas@outlook.com>
2016-11-24 23:15:48 +0000

    Add pubdate to TorrentProject

--- medusa/providers/torrent/html/torrentproject.py
+++ medusa/providers/torrent/html/torrentproject.py
@@ -18,7 +18,10 @@
 """Provider code for TorrentProject."""
 from __future__ import unicode_literals
 
+import datetime
 import traceback
+
+from pytimeparse import parse
 
 import validators
 
@@ -149,13 +152,17 @@ class TorrentProjectProvider(TorrentProvider):
                     torrent_size = row.find('span', class_='bc torrent-size').get_text().rstrip()
                     size = convert_size(torrent_size) or -1
 
+                    pubdate_raw = row.find('span', class_='bc cated').get_text()[:-4]
+                    pubdate = str(datetime.datetime.now() - datetime.timedelta(seconds=parse(pubdate_raw)))\
+                        if pubdate_raw else None
+
                     item = {
                         'title': title,
                         'link': download_url,
                         'size': size,
                         'seeders': seeders,
                         'leechers': leechers,
-                        'pubdate': None,
+                        'pubdate': pubdate,
                     }
                     if mode != 'RSS':
                         logger.log('Found result: {0} with {1} seeders and {2} leechers'.format

81cd661e4ab7677366fde5041272f2a74414bf4b
supergonkas <matigonkas@outlook.com>
2016-12-14 17:56:14 +0000

    rarbg tvdbid for non tvdb shows

--- medusa/providers/torrent/json/rarbg.py
+++ medusa/providers/torrent/json/rarbg.py
@@ -96,7 +96,9 @@ class RarbgProvider(TorrentProvider):
             else:
                 search_params['sort'] = self.sorting if self.sorting else 'seeders'
                 search_params['mode'] = 'search'
-                search_params['search_tvdb'] = self._get_tvdb_id()
+                tvdb_id = self._get_tvdb_id()
+                if tvdb_id:
+                    search_params['search_tvdb'] = tvdb_id
 
             for search_string in search_strings[mode]:
                 if mode != 'RSS':
